f344c206b5cf9a973a4dbf4f804cbdb50979af49
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-27 09:13:30 +0700

    Create hey.sh

---
+++ hey.sh
@@ -0,0 +1,82 @@
+#colorsblue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+lightgreen='\e[1;32m'
+
+figlet -f slant "MR_XiD" |lolcat
+echo                    " [======<>======<>======<>======]"|lolcat
+echo                    " [Author   :  MR_XiD            ]"|lolcat
+echo                    " [Team     : D35TR0Y SQUAD      ]"|lolcat
+echo                    " [Note     : Jangan reqode ya   ]"|lolcat
+echo                    " [Thansk to: All member         ]"|lolcat
+echo                    " [thanks to: JATIM BLACKHAT     ]"|lolcat
+echo                    " [Note2    : Copyright©MR_XID   ]"|lolcat
+echo                    " [======<>======<>======<>======]"|lolcat
+
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    "1.) SANTET ONLINE "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    "2.) TOOL MR_XID "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    "
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    " 
+echo    "<•¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤•>"|lolcat
+echo    "╭─[PILIH NOMORNYA]"
+read -p "╰─root@./MR_XID=" ff;
+
+if [ $ff = 1 ] || [ $ff = 1 ]
+then
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+echo "terinstall pK"
+fi
+if [ $ff = 2 ] || [ $ff = 2]
+then
+pkg install git
+git clone https://github.com/safri1234/COMANDMR_XID
+cd COMANDMR_XID
+sleep 1
+sh COMAND.sh
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
